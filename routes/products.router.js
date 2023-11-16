4b4b176bd68d591a880d996fde8ebeaf26e29f5c
Junsik <chzhgod@gmail.com>
2023-11-16 21:24:34 +0900

    Feat: [main] 상품삭제 기능 추가
    1)  상품 삭제 기능
    - 유효성 검사
    * 해당 상품이 없을 경우 404 코드 및 에러 메시지 반환
    * 해당 상품이 본인 것이 아닐 경우

--- routes/products.router.js
+++ routes/products.router.js
@@ -7,18 +7,21 @@ const { Products } = db;
 const router = express.Router();
 
 
-// Create 
+// Create 상품 등록
 router.post('/products', authMiddleware, async (req, res) => {
   try {
     const { id } = res.locals.user;
-    console.log('id: ', typeof id);
     const { productName, contents } = req.body;
+
+
     if (!req.body) { return res.status(400).json({ errorMessage: " 데이터 형식이 올바르지 않습니다. " }) };
 
+    // 데이터가 모두 입력되지 않았을 경우
     if (!productName || !contents) {
-      return res.status(400).json({ errorMessage: " 데이터를 모두 입력해주세요. " });
+      return res.status(400).json({ errorMessage: "데이터를 모두 입력해주세요." });
     }
 
+    // 상품 생성 (생성 시 userId는 res.locals.user의 id값을 가져온다.)
     await Products.create({ userId: id, productName, contents })
 
     res.status(201).json({ message: "상품등록에 성공하였습니다." });
@@ -73,14 +76,17 @@ router.put("/products/detail/:productId", authMiddleware, async (req, res) => {
 
     const existsProduct = await Products.findOne({ where: { id: productId } });
 
+    // 수정할 상품이 존재하지 않을 경우 에러메시지 변환
     if (!existsProduct) {
       return res.status(404).json({ errorMessage: "상품을 찾을 수 없습니다." });
     };
 
+    // 기존 작성한 상품이 본인 것이 아닐 경우
     if (existsProduct.id !== userId) {
       return res.status(401).json({ errorMessage: "상품을 수정할 권한이 없습니다." });
     };
 
+    // 상품 수정하기
     await Products.update(
       { productName, contents, status },
       {
@@ -89,9 +95,6 @@ router.put("/products/detail/:productId", authMiddleware, async (req, res) => {
         }
       }
     );
-
-
-    // 수정 성공 시 200 상태 코드 반환
     res.status(200).json({ message: '상품 수정이 완료되었습니다.' });
   } catch (error) {
     console.error('상품 수정 실패', error);
@@ -107,24 +110,32 @@ router.put("/products/detail/:productId", authMiddleware, async (req, res) => {
 
 // Delete
 // 상품 삭제
-router.delete("/products/detail/:productId", async (req, res) => {
+router.delete("/products/detail/:productId", authMiddleware, async (req, res) => {
   try {
     const { productId } = req.params;
-    const { password } = req.body;
+    const { id: userId } = res.locals.user; // 로컬 유저에 들어있는 id 키값을 userId 변수로 받는다.
 
-    const existsProduct = await Products.findOne({ _id: productId });
+    const existsProduct = await Products.findOne({ where: { id: productId } });
 
+    // 해당 상품을 찾을 수 없을 경우
     if (!existsProduct) {
       return res.status(404).json({ message: "상품 조회에 실패하였습니다." })
     }
 
-    if (existsProduct.password !== password) {
-      return res.status(401).json({ message: "비밀번호를 확인해주세요." });
-    }
+    // 기존 작성한 상품이 본인 것이 아닐 경우
+    if (existsProduct.id !== userId) {
+      return res.status(401).json({ errorMessage: "상품을 삭제할 권한이 없습니다." });
+    };
 
     // 비밀번호가 일치하고 상품이 존재하는 경우에만 삭제 수행
-    await Products.deleteOne({ _id: productId })
+    await Products.destroy({
+      where: {
+        [Op.and]: [{ id: productId }, { userId }]
+      }
+    });
     res.status(200).json({ message: "상품이 삭제되었습니다." })
+
+
   } catch (error) {
     console.error('상품 삭제 실패', error);
     res.status(500).json('상품 삭제에 실패했습니다.');
