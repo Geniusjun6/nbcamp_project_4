8446d83b242428616673b58a606e383076e057e1
Junsik <chzhgod@gmail.com>
2023-12-06 15:21:45 +0900

    docs: [main] 프로젝트 진행을 위한 세팅

---
+++ src/routers/users.router.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import db from '../models/index.cjs';
+import authMiddleware from '../middlwares/need-signin.middlware.js';
+
+const { Users } = db;
+const router = express.Router();
+
+// Read 사용자 조회 // 
+router.get("/users/userInfo", authMiddleware, async (req, res) => {
+  try {
+    const { id } = res.locals.user;
+
+    const user = await Users.findOne({
+      attributes: { exclude: ["password"] },
+      where: { id }
+    });
+
+    return res.status(200).json({ user });
+
+  } catch (error) {
+    console.error("사용자 조회 실패", error);
+    res.status(500).json({ errorMessage: "사용자 조회에 실패했습니다." });
+  }
+})
+
+export default router;
