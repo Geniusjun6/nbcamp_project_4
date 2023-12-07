da967763b57cea521d6df74d066f7b115cadb823
Junsik <chzhgod@gmail.com>
2023-12-07 17:48:20 +0900

    feat: [errorMiddleware] 에러 핸들링 미들웨어 활용하여 유효성 검사
    - 유효성 검사 시 throw new Error로 에러 메시지 발생
    - 에러 핸들링 미들웨어에서 에러 메시지에 따라 상태값 변경 및 동일한 에러 메시지 반환

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cookieParser from "cookie-parser";
 import productsRouter from "./routers/products.router.js";
 import userRouter from "./routers/users.router.js";
+import errorHandling from "./middlewares/error.middleware.js";
 
 import dotenv from 'dotenv'; // .env 패키지를 사용하기 위해 불러오고 실행함
 dotenv.config();
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api', [userRouter, productsRouter]);
+
+app.use(errorHandling);
 
 
 app.listen(port, () => {
